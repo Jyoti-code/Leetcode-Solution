0c63cafadf872ee1ce852729c76144c348701a09
Jyoti-code <jyotisharmarkt2001@gmail.com>
2023-03-30 15:46:21 +0530

    30-03-2023

---
+++ 83. Merge Sorted Array.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/merge-sorted-array/description/
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m,m+n):
+            nums1[i]=nums2[i-m]
+        return nums1.sort()
