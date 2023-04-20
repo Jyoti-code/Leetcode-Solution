c04c6284f3a2a108c71c820ba361634b7df1432b
Jyoti-code <jyotisharmarkt2001@gmail.com>
2023-04-20 14:04:08 +0530

    20-04-2023

---
+++ 108. Duplicate Zeros.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/duplicate-zeros/description/
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i=0
+        while i<len(arr):
+            if arr[i]==0:
+                arr.insert(i+1,0)
+                arr.pop()
+                i+=2
+            else:
+                i+=1
+        return arr
+        
+        # pass only 23 test cases
+        
+        for i in range(len(arr)-1,0,-1):
+            if arr[i]==0:
+                arr.insert(i+1,0)
+                arr.pop()
+        return arr
