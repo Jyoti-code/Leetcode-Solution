9040c90be1888de490e4b42a4d742582f6ccea49
Jyoti-code <jyotisharmarkt2001@gmail.com>
2023-05-06 20:41:35 +0530

    06-05-2023

---
+++ 123. Group Sold Products By The Date.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/group-sold-products-by-the-date/description/
+
+# Write your MySQL query statement below
+select sell_date,count(distinct(product)) as num_sold, 
+group_concat(distinct(product)) as products  from Activities group by sell_date;
