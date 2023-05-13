24c717f2b1e9bcc7b62ea0618fde46f8f2bd15be
Jyoti-code <jyotisharmarkt2001@gmail.com>
2023-05-13 14:18:41 +0530

    13-05-2023

---
+++ 129.  Sum of All Subset XOR Totals.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/not-boring-movies/description/
+
+# Write your MySQL query statement below
+select *from Cinema where id%2!=0 and description!="boring" order by rating desc;
