038ef6438121c3909c8d7d7622a85d477810bc7d
Jyoti-code <jyotisharmarkt2001@gmail.com>
2023-05-19 13:53:08 +0530

    19-04-2001

---
+++ 134. Invalid Tweets.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&id=top-sql-50
+
+# Write your MySQL query statement below
+select tweet_id from Tweets where length(content)>15;
