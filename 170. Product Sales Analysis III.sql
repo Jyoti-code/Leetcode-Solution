ed3503d72b94a81e2b73316d69023b7a67364357
Jyoti-code <jyotisharmarkt2001@gmail.com>
2023-07-06 11:20:01 +0530

    06-07-2023

---
+++ 170. Product Sales Analysis III.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/product-sales-analysis-iii/description/
+
+# Write your MySQL query statement below
+select  product_id,year as first_year,quantity,price from Sales
+where (product_id,year) in
+ (select product_id,min(year) as year from Sales group by product_id);
